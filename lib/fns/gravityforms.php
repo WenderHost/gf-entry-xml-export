2c57378435f49c7d7e94200c0513b48255d59bfd
Michael Wender <michael@michaelwender.com>
2017-05-05 11:16:54 -0400

    Filtering $new_keys

--- lib/fns/gravityforms.php
+++ lib/fns/gravityforms.php
@@ -45,18 +45,15 @@ function gforms_after_submission( $entry, $form )
     }
 
     // Rename keys
-    $new_keys = [
-        'First' => 'first_name',
-        'Last' => 'last_name',
-        'Cancer Diagnosis:' => 'type_of_cancer',
-        'Cancer Diagnosis or Additional Comments:' => 'comments',
-        'How can we help?' => 'request_type',
-    ];
-
-    foreach( $data as $key => $value ){
-        if( array_key_exists( $key, $new_keys ) ){
-            $data[$new_keys[$key]] = $value;
-            unset( $data[$key] );
+    $new_keys = [];
+    $new_keys = apply_filters( 'gf_to_xml_array_keys', $new_keys, $entry['form_id'] );
+    if( ! empty( $new_keys ) && is_array( $new_keys ) )
+    {
+        foreach( $data as $key => $value ){
+            if( array_key_exists( $key, $new_keys ) ){
+                $data[$new_keys[$key]] = $value;
+                unset( $data[$key] );
+            }
         }
     }
 
