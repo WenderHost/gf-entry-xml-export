ec7f38288bee8074615b2540be2e0f43fac20a7b
Michael Wender <michael@michaelwender.com>
2017-05-05 11:26:47 -0400

    Documentation, Bumping version no.

--- gf-entry-xml-export.php
+++ gf-entry-xml-export.php
@@ -2,12 +2,12 @@
 /**
  * Plugin Name:     Gravity Forms Entry XML Export for Provision IMS
  * Plugin URI:      https://github.com/mwender
- * Description:     Writes out Gravity Forms entries as XML files for later import into Provision Healthcare's Intake Management System
+ * Description:     Writes out Gravity Forms entries as XML files
  * Author:          Michael Wender
  * Author URI:      https://michaelwender.com
  * Text Domain:     gf-entry-xml-export
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         1.0.0
  *
  * @package         Gf_Entry_Xml_Export
  */
