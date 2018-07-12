469c4d8e59a97c55208e01a01800ba326dc8e7db
Michael Wender <michael@michaelwender.com>
2018-07-12 13:51:34 -0400

    Adding file and line to write_log() output

--- lib/fns/files.php
+++ lib/fns/files.php
@@ -14,7 +14,7 @@ function write_file( $file_contents = '', $filename = 'file.txt', $dir = 'tmp' )
         // break if we find any problems
         if( ! \WP_Filesystem( $creds ) )
         {
-            write_log( 'Unable to get filesystem credentials.' );
+            write_log( 'Unable to get filesystem credentials.', 'GFtoXML::' . basename(__FILE__) . '::' . __LINE__ );
             return false;
         }
 
@@ -26,20 +26,20 @@ function write_file( $file_contents = '', $filename = 'file.txt', $dir = 'tmp' )
 
         if( ! $wp_filesystem->is_dir( $target_dir ) )
         {
-            write_log( 'Unable to create XML directory (' . $target_dir . ').' );
+            write_log( 'Unable to create XML directory (' . $target_dir . ').', 'GFtoXML::' . basename(__FILE__) . '::' . __LINE__ );
             return false;
         }
 
         // Prevent web access via .htaccess
         $ht_file = $target_dir . '.htaccess';
         if( ! $wp_filesystem->exists( $ht_file ) ){
-            write_log( 'Writing .htaccess file...' );
+            write_log( 'Writing .htaccess file...', 'GFtoXML::' . basename(__FILE__) . '::' . __LINE__ );
             $contents = "# Prevent access\nDeny from all";
             $wp_filesystem->put_contents( $ht_file, $contents, 0444 );
         }
 
         $file_to_write = $target_dir . $filename;
-        write_log( 'Writing file ' . $file_to_write );
+        write_log( 'Writing file ' . $file_to_write, 'GFtoXML::' . basename(__FILE__) . '::' . __LINE__ );
         $wp_filesystem->put_contents( $file_to_write, $file_contents );
     }
 }
