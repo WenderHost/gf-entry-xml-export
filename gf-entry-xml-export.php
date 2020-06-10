424c59a5034eebfda9931c292407877a7e81eb95
Michael Wender <michael@michaelwender.com>
2020-06-10 10:19:14 -0400

    Processing "Skills"

--- gf-entry-xml-export.php
+++ gf-entry-xml-export.php
@@ -38,6 +38,26 @@ function digicube_filter_data( $data ){
         $data[$key] = $date->format('d.m.Y');
         break;
     }
+
+    if( stristr( $key, 'skill') ){
+      /**
+       * Spilt keys like `skill_1_id`, `skill_2_rating`, etc into an array as follows:
+       * 0 - `skill`
+       * 1 - Skill number
+       * 2 - `id` or `rating`
+       */
+      $key_array = explode( '_', $key );
+      if( is_array( $key_array ) && 3 == count( $key_array ) ){
+        $data['skills']['skill' . $key_array[1] ][$key_array[2]] = $value;
+      }
+
+      // If we have both skill_X_id and skill_X_rating inside `skills`, we can delete the corresponding
+      // `skill_X_id` and `skill_X_rating` from $data:
+      $skill_check_array = $data['skills']['skill' . $key_array[1] ];
+      if( array_key_exists( 'id', $skill_check_array) && array_key_exists( 'rating', $skill_check_array ) )
+        unset( $data['skill_' . $key_array[1]. '_id'], $data['skill_' . $key_array[1]. '_rating'] );
+    }
+
   }
 
   return $data;
@@ -45,8 +65,8 @@ function digicube_filter_data( $data ){
 add_filter( 'gf_to_xml_filter_data', 'digicube_filter_data' );
 
 function digicube_filter_xml( $data ){
-  $search = ['document0','document1'];
-  $replace = ['document','document'];
+  $search = ['document0','document1','document2','document3','document4','document5','skill1','skill2','skill3','skill4','skill5','skill6'];
+  $replace = ['document','document','document','document','document','document','skill','skill','skill','skill','skill','skill'];
   $data = str_replace( $search, $replace, $data );
   return $data;
 }
